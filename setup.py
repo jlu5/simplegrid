9d31d29e38cf7e7f2428b255970950669d59192f
James Lu <james@overdrivenetworks.com>
2018-08-28 16:09:41 -0700

    Add a simple setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+VERSION = "0.1"
+
+setup(
+    name='simplegrid',
+    version=VERSION,
+
+    description='Simple 2D grid systems',
+
+    url='https://github.com/jlu5/simplegrid',
+
+    author='James Lu',
+    author_email='james@overdrivenetworks.com',
+    
+    license='GPL 3',
+    
+    packages=find_packages()
+)
